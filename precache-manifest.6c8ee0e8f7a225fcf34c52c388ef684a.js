d207eb9022cbd9e7b8868dabdddabf703bf903a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-15 00:56:42 +0000

    Deploy dwong-brighter/bc-pst-aws-calc-reactjs to github.com/dwong-brighter/bc-pst-aws-calc-reactjs.git:gh-pages

---
+++ precache-manifest.6c8ee0e8f7a225fcf34c52c388ef684a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07da97cf721e8232042f3527ff376fb0",
+    "url": "/bc-pst-aws-calc-reactjs/index.html"
+  },
+  {
+    "revision": "b41f5e6a5a8a1225fc06",
+    "url": "/bc-pst-aws-calc-reactjs/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "4119c62c524789fa1e25",
+    "url": "/bc-pst-aws-calc-reactjs/static/js/2.d03185ad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bc-pst-aws-calc-reactjs/static/js/2.d03185ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b41f5e6a5a8a1225fc06",
+    "url": "/bc-pst-aws-calc-reactjs/static/js/main.b93bf34d.chunk.js"
+  },
+  {
+    "revision": "57f397667297f5c14ece",
+    "url": "/bc-pst-aws-calc-reactjs/static/js/runtime-main.389419ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bc-pst-aws-calc-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
